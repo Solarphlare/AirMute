6ecdabff6d84da65c97a88d8a513d1a67d6e4e9e
William M <cominatyou@cominatyou.com>
2026-04-13 10:22:41 -0500

    Increase size of avatar in sidebar

--- AirMute/Settings Views/SettingsSidebarView.swift
+++ AirMute/Settings Views/SettingsSidebarView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct SettingsSidebarView: View {
     @Binding var selectedPage: SettingsPage
     @AppStorage("update_available") private var updateAvailable = false
+    let SIDEBAR_AVATAR_SIZE: CGFloat = 40
     
     var body: some View {
         let user = (NSApplication.shared.delegate as! AppDelegate).rpc?.user
@@ -17,22 +18,23 @@ struct SettingsSidebarView: View {
                             CachedAsyncImage(url: url) {
                                 $0.resizable()
                                     .clipShape(Circle())
-                                    .frame(width: 30, height: 30)
+                                    .frame(width: SIDEBAR_AVATAR_SIZE, height: SIDEBAR_AVATAR_SIZE)
                             } placeholder: {
                                 Image(systemName: "person.crop.circle.fill")
-                                    .font(.system(size: 30))
-                                    .frame(width: 30, height: 30)
+                                    .font(.system(size: SIDEBAR_AVATAR_SIZE))
+                                    .frame(width: SIDEBAR_AVATAR_SIZE, height: SIDEBAR_AVATAR_SIZE)
                             }
                         }
                         else {
                             Image(systemName: "person.crop.circle.fill")
-                                .font(.system(size: 30))
-                                .frame(width: 30, height: 30)
+                                .font(.system(size: SIDEBAR_AVATAR_SIZE))
+                                .frame(width: SIDEBAR_AVATAR_SIZE, height: SIDEBAR_AVATAR_SIZE)
                         }
                         
-                        VStack(alignment: .leading) {
+                        VStack(alignment: .leading, spacing: 2) {
                             Text(user.globalName)
                                 .fontWeight(.semibold)
+                                .font(.system(size: 13.5))
                             Text("@" + user.username)
                                 .font(.system(size: 11.5))
                                 .opacity(0.5)
