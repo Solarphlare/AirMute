8c801e63be77fa47e6d10eece2464893781c2fe6
William M <cominatyou@cominatyou.com>
2026-04-13 10:26:35 -0500

    Drop RPC connection when system goes to sleep

--- AirMute/Observers.swift
+++ AirMute/Observers.swift
@@ -1,11 +1,11 @@
 import AppKit
+import AVFoundation
 
 extension AppDelegate {
     func initObservers(_ rpc: RPC) {
         NSWorkspace.shared.notificationCenter.addObserver(forName: NSWorkspace.didLaunchApplicationNotification, object: nil, queue: nil) { notif in
             if let app = notif.userInfo?[NSWorkspace.applicationUserInfoKey] as? NSRunningApplication {
                 if app.bundleIdentifier == "com.hnc.Discord" {
-                    self.statusItemTitle = String(localized: "Trying to connect...")
                     logger.info("Discord is open.")
                     self.connectToDiscord(rpc)
                 }
@@ -17,7 +17,30 @@ extension AppDelegate {
                 if app.bundleIdentifier == "com.hnc.Discord" {
                     self.statusItemTitle = String(localized: "Inactive — Discord Not Open")
                     self.controller?.stop()
-                    self.rpc?.closeSocket()
+                    rpc.closeSocket()
+                }
+            }
+        }
+        
+        NSWorkspace.shared.notificationCenter.addObserver(forName: NSWorkspace.willSleepNotification, object: nil, queue: nil) { _ in
+            self.statusItemTitle = String(localized: "Inactive — System Asleep")
+            self.controller?.stop()
+            rpc.closeSocket()
+        }
+        
+        NSWorkspace.shared.notificationCenter.addObserver(forName: NSWorkspace.didWakeNotification, object: nil, queue: nil) { _ in
+            guard let _ = AVCaptureDevice.default(for: .audio) else {
+                self.isMicrophoneConnected = false
+                return
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now().advanced(by: .seconds(15))) {
+                if NSRunningApplication.runningApplications(withBundleIdentifier: "com.hnc.Discord").isEmpty {
+                    self.statusItemTitle = String(localized: "Inactive — Discord Not Open")
+                }
+                else {
+                    self.controller?.start()
+                    self.connectToDiscord(rpc)
                 }
             }
         }
