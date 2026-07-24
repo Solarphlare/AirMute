9fe76c0ad4ab848acc9445a37a987f6732adb968
William M <cominatyou@cominatyou.com>
2026-07-24 13:40:56 +1000

    Remove sleep notifications

--- AirMute/Observers.swift
+++ AirMute/Observers.swift
@@ -21,28 +21,5 @@ extension AppDelegate {
                 }
             }
         }
-        
-        NSWorkspace.shared.notificationCenter.addObserver(forName: NSWorkspace.willSleepNotification, object: nil, queue: nil) { _ in
-            self.statusItemTitle = String(localized: "Inactive — System Asleep")
-            self.controller?.stop()
-            rpc.closeSocket()
-        }
-        
-        NSWorkspace.shared.notificationCenter.addObserver(forName: NSWorkspace.didWakeNotification, object: nil, queue: nil) { _ in
-            guard let _ = AVCaptureDevice.default(for: .audio) else {
-                self.isMicrophoneConnected = false
-                return
-            }
-            
-            DispatchQueue.main.asyncAfter(deadline: .now().advanced(by: .seconds(15))) {
-                if NSRunningApplication.runningApplications(withBundleIdentifier: "com.hnc.Discord").isEmpty {
-                    self.statusItemTitle = String(localized: "Inactive — Discord Not Open")
-                }
-                else {
-                    self.controller?.start()
-                    self.connectToDiscord(rpc)
-                }
-            }
-        }
     }
 }
